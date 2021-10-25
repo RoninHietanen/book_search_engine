0d53b0a146190262d3be8208d7d185b18f0629c5
RoninHietanen <84194778+RoninHietanen@users.noreply.github.com>
2021-10-25 23:30:47 +1100

    ""

--- server/config/connection.js
+++ server/config/connection.js
@@ -7,4 +7,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book_search_eng
   useFindAndModify: false,
 });
 
+mongoose.connection.on('error', (err)=>{
+  console.log(err);
+});
+
 module.exports = mongoose.connection;
